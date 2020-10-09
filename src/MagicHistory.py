361576a240f9060db4b916d4e3d122cbeb586c77
leapdr <aron42610@hotmail.com>
2020-10-09 12:13:33 +0800

    UPDATED history functions and formats

--- src/MagicHistory.py
+++ src/MagicHistory.py
@@ -13,10 +13,13 @@ class MagicHistory(object):
         pass
 
     def _historyCounter(func):
-        def fn(self):
-            func(self)
+        def fn(self, *args, **kwargs):
+            data = func(self, *args, **kwargs)
+
             self.currentSize = len(self.current)
             self.solutionsSize = len(self.solutions)
+
+            return data
         return fn
 
     # get From current
@@ -29,7 +32,13 @@ class MagicHistory(object):
     def addToCurrent(self, entry):
         self.current.insert(0, entry)
 
+    # get from solutions
+    @_historyCounter
+    def getFromSolutions(self, index = 0):
+        return self.solutions.pop(index)
+
     # add solution to History
+    # format (equation, answer)
+    @_historyCounter
     def addSolution(self, entry):
-        
-        pass
+        self.solutions.insert(0, entry)
