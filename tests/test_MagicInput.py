699a5ed4ef0e3869df394922bd5f28bd1aa0551a
Aaron Basco <aron42610@hotmail.com>
2020-12-13 16:07:47 +0800

    ADDED some test cases for MagicInput validate method

--- tests/test_MagicInput.py
+++ tests/test_MagicInput.py
@@ -6,3 +6,22 @@ class TestMagicInput(unittest.TestCase):
     # Test if is_integer returns correct value
     magic_input = MagicInput("2")
     self.assertTrue(magic_input.isInteger())
+
+  def test_validate_valid_expressions(self):
+    # Test validate method for some valid expressions
+    valid_expressions = [
+      "3(4+2(7/3)*1)", # basic arithmetic
+      "2cos sin tan (.3sin(.2))", # trigonometric
+    ]
+
+    for expr in valid_expressions:
+      magic_input = MagicInput(expr)
+
+      # validate returns empty string if expression is valid
+      self.assertEqual(magic_input.validate(), "")
+
+  def test_validate_invalid_expressions(self):
+    # Test validate method for some invalid expressions
+    invalid_expressions = [
+
+    ]
